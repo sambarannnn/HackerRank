6cd0ce49d7a87020cf91b8c23e25afddc3b8bdeb
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-06 19:34:42 +0530

    Create Solution.java

---
+++ DS/Linked Lists/Cycle Detection/Solution.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+
+    static boolean hasCycle(SinglyLinkedListNode head) {
+        SinglyLinkedListNode p = head;
+        SinglyLinkedListNode q = head;
+        //moving p by 2 indexes
+        //moving q by 1 index
+        //if it has a cycle, it is bound to meet at some point
+        while(p != null && q != null) {
+            p = p.next;
+            q = q.next;
+            if(p!=null) 
+                p = p.next;
+            if(p == q) 
+                return true;
+        }
+        return false;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
