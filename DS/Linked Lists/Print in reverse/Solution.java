bd40a56e7eef18f2ab94355fe8573f279ff4630c
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-06 13:47:03 +0530

    Create Solution.java

---
+++ DS/Linked Lists/Print in reverse/Solution.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data);
+
+            node = node.next;
+
+            if (node != null) {
+                System.out.print(sep);
+            }
+        }
+    }
+
+
+    public static void reversePrint(SinglyLinkedListNode llist) {
+    // Write your code here
+        SinglyLinkedListNode temp = llist;
+        if(temp == null) {
+            return;
+        } else {
+            reversePrint(temp.next);
+            System.out.println(temp.data);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
