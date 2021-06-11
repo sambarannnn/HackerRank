bdccb616d1400256331345d5800dfb5c503ba989
Sambaran Sengupta <63100608+sambarannnn@users.noreply.github.com>
2021-06-11 11:24:37 +0530

    Update Solution.java

--- DS/Stacks/Simple Text Editor/Solution.java
+++ DS/Stacks/Simple Text Editor/Solution.java
@@ -11,26 +11,26 @@ public class Solution {
         Scanner s=new Scanner(System.in);
         int Q = s.nextInt();
         String currentString = "";
-        Stack<String> st =new Stack<String>();
-        st.push(currentString);
+        Stack<String> StringStack =new Stack<String>();
+        StringStack.push(currentString);
         StringBuilder printfinal = new StringBuilder();
         while(Q-- > 0) {
             int query = s.nextInt();
             switch(query) {
                 case 1:
                     currentString = currentString + s.next();
-                    st.push(currentString);
+                    StringStack.push(currentString);
                     break;
                 case 2:
-        currentString = currentString.substring(0,currentString.length()-s.nextInt());
-                    st.push(currentString);
+                    currentString = currentString.substring(0,currentString.length()-s.nextInt());
+                    StringStack.push(currentString);
                     break;
                 case 3:
                     printfinal.append(currentString.charAt(s.nextInt()-1) + "\n");
                     break;
                 case 4:
-                    st.pop();
-                    currentString=st.peek();
+                    StringStack.pop();
+                    currentString=StringStack.peek();
             } 
         }
         System.out.println(printfinal.toString());
